fbfefd35091932dd2cb293f53acbf811b0c75d39
Jeevan1351 <vijayeshjeevan.cs19@bmsce.ac.in>
2022-01-13 11:06:45 +0530

    Minor changes

--- temp_convert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "This program is to convert temperature from farenheit to celsius"
-echo "Enter the temperature in farenheit"
-read tempF
-tempC=`echo "($tempF-32)*5/9"|bc`
-echo "The temperature in celsius is $tempC"
-
